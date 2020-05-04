eab66e4531a99d9625cb7ff944bfb0d75800acaf
princeron77 <amangola135@gmail.com>
2020-05-04 18:40:58 +0530

    First Commit to check git is working...

---
+++ leetCode_MayChallenge/src/leetCode_MayChallenge/CheckStatus.java
@@ -0,0 +1,10 @@
+package leetCode_MayChallenge;
+
+public class CheckStatus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git is working");
+	}
+
+}
